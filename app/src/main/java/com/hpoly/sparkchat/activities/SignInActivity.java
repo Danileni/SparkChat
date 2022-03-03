fc5e7666e5d5bd72514957840b31149bef5abc55
Eleni Polikrati <56802399+Danileni@users.noreply.github.com>
2022-03-03 02:26:49 +0200

    second commit

---
+++ app/src/main/java/com/hpoly/sparkchat/activities/SignInActivity.java
@@ -0,0 +1,15 @@
+package com.hpoly.sparkchat.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.hpoly.sparkchat.R;
+
+public class SignInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+    }
+}
