c64583eb09c09a0f267475a7271e5b306d4ce4f2
Eleni Polikrati <56802399+Danileni@users.noreply.github.com>
2022-03-03 16:47:59 +0200

    basic layouts, enabling view binding

--- app/src/main/java/com/hpoly/sparkchat/activities/SignInActivity.java
+++ app/src/main/java/com/hpoly/sparkchat/activities/SignInActivity.java
@@ -1,15 +1,30 @@
 package com.hpoly.sparkchat.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.hpoly.sparkchat.R;
+import com.hpoly.sparkchat.databinding.ActivitySignInBinding;
 
 public class SignInActivity extends AppCompatActivity {
+
+    // As we’ve enabled viewBinding for our project, the binding class for each XML layout will be generated automatically.
+    // Here ‘ActivitySignInBinding’ class is automatically generated from our layout file: ‘activity_sign_in’
+    private ActivitySignInBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_in);
+        binding = ActivitySignInBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        setListeners();
+    }
+
+    private void setListeners() {
+        // an instance of a binding class contains direct references to all views that have an ID in the corresponding layout
+        binding.CreateNewAccount.setOnClickListener(v ->
+                startActivity(new Intent(getApplicationContext(), SignUpActivity.class)));
     }
 }
--- app/src/main/java/com/hpoly/sparkchat/activities/SignUpActivity.java
+++ app/src/main/java/com/hpoly/sparkchat/activities/SignUpActivity.java
@@ -5,12 +5,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.hpoly.sparkchat.R;
+import com.hpoly.sparkchat.databinding.ActivitySignUpBinding;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    private ActivitySignUpBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up);
+        binding = ActivitySignUpBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        setListeners();
+    }
+
+    private void setListeners() {
+        binding.textSignIn.setOnClickListener(v -> onBackPressed());
     }
 }
